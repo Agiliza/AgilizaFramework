723dc7a4d0e8b6a78abaabe9c0240c07f236bb4b
Vicente Ruiz <vruiz2.0@gmail.com>
2012-02-06 15:24:21 +0100

    Some links and documentation

--- agiliza/net/http/request.py
+++ agiliza/net/http/request.py
@@ -1,3 +1,44 @@
+"""
+See http://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html#sec4.5 and
+    http://www.w3.org/Protocols/rfc2616/rfc2616-sec5.html#sec5.3
+
+4.5 General Header Fields
+
+    Cache-Control            ; Section 14.9
+    Connection               ; Section 14.10
+    Date                     ; Section 14.18
+    Pragma                   ; Section 14.32
+    Trailer                  ; Section 14.40
+    Transfer-Encoding        ; Section 14.41
+    Upgrade                  ; Section 14.42
+    Via                      ; Section 14.45
+    Warning                  ; Section 14.46
+
+5.3 Request Header Fields
+
+    Accept                   ; Section 14.1
+    Accept-Charset           ; Section 14.2
+    Accept-Encoding          ; Section 14.3
+    Accept-Language          ; Section 14.4
+    Authorization            ; Section 14.8
+    Expect                   ; Section 14.20
+    From                     ; Section 14.22
+    Host                     ; Section 14.23
+    If-Match                 ; Section 14.24
+
+    If-Modified-Since        ; Section 14.25
+    If-None-Match            ; Section 14.26
+    If-Range                 ; Section 14.27
+    If-Unmodified-Since      ; Section 14.28
+    Max-Forwards             ; Section 14.31
+    Proxy-Authorization      ; Section 14.34
+    Range                    ; Section 14.35
+    Referer                  ; Section 14.36
+    TE                       ; Section 14.39
+    User-Agent               ; Section 14.43
+
+"""
+
 class HttpRequest(object):
     """A basic HTTP request."""
     pass
--- agiliza/net/http/response.py
+++ agiliza/net/http/response.py
@@ -1,3 +1,48 @@
+"""
+See http://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html#sec4.5,
+    http://www.w3.org/Protocols/rfc2616/rfc2616-sec6.html#sec6.2 and
+    http://www.w3.org/Protocols/rfc2616/rfc2616-sec7.html#sec7.1
+
+4.5 General Header Fields
+
+    Cache-Control            ; Section 14.9
+    Connection               ; Section 14.10
+    Date                     ; Section 14.18
+    Pragma                   ; Section 14.32
+    Trailer                  ; Section 14.40
+    Transfer-Encoding        ; Section 14.41
+    Upgrade                  ; Section 14.42
+    Via                      ; Section 14.45
+    Warning                  ; Section 14.46
+
+6.2 Response Header Fields
+
+    Accept-Ranges           ; Section 14.5
+    Age                     ; Section 14.6
+    ETag                    ; Section 14.19
+    Location                ; Section 14.30
+    Proxy-Authenticate      ; Section 14.33
+
+    Retry-After             ; Section 14.37
+    Server                  ; Section 14.38
+    Vary                    ; Section 14.44
+    WWW-Authenticate        ; Section 14.47
+
+7.1 Entity Header Fields
+
+    Allow                    ; Section 14.7
+    Content-Encoding         ; Section 14.11
+    Content-Language         ; Section 14.12
+    Content-Length           ; Section 14.13
+    Content-Location         ; Section 14.14
+    Content-MD5              ; Section 14.15
+    Content-Range            ; Section 14.16
+    Content-Type             ; Section 14.17
+    Expires                  ; Section 14.21
+    Last-Modified            ; Section 14.29
+
+
+"""
 import abc
 
 
