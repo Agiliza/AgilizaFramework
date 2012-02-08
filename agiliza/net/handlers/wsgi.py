86066ff317c03a1fcc36157363106150eae75b1c
Vicente Ruiz <vruiz2.0@gmail.com>
2012-02-08 15:30:05 +0100

    WSGI

--- agiliza/net/handlers/wsgi.py
+++ agiliza/net/handlers/wsgi.py
@@ -20,32 +20,31 @@ from agiliza.net.handlers import base
 
 class WSGIRequest(http.HttpRequest):
     def __init__(self, environ):
-        script_name = environ.get('SCRIPT_NAME')
-        path_info = environ.get('PATH_INFO', '/')
-        if not path_info or path_info == script_name:
-            # Sometimes PATH_INFO exists, but is empty (e.g. accessing
-            # the SCRIPT_NAME URL without a trailing slash). We really need to
-            # operate as if they'd requested '/'. Not amazingly nice to force
-            # the path like this, but should be harmless.
-            path_info = '/'
-        self.environ = environ
-        self.path_info = path_info
-        self.path = '%s%s' % (script_name, path_info)
-        self.method = environ['REQUEST_METHOD'].upper()
-        self._stream = self.environ['wsgi.input']
+        """Wrap a WSGI environ dictionary."""
+        self.META = environ
+        self._method = environ['REQUEST_METHOD'].upper()
+
+    @property
+    def method(self):
+        return self._method
+
+    @property
+    def path_info(self):
+        return '/' + self.META.get('PATH_INFO','').lstrip('/')
+
+    @property
+    def query_string(self):
+        return '/' + self.META.get('QUERY_STRING','')
+
+    @property
+    def script_name(self):
+        return self.environ.get('SCRIPT_NAME', '')
 
     def is_secure(self):
         return 'wsgi.url_scheme' in self.environ \
             and self.environ['wsgi.url_scheme'] == 'https'
 
-    def get_host(environ):
-        if 'HTTP_HOST' in environ:
-            return environ['HTTP_HOST']
-        result = environ['SERVER_NAME']
-        if (environ['wsgi.url_scheme'], environ['SERVER_PORT']) not \
-           in ((b'https', b'443'), (b'http', b'80')):
-            result += b':' + environ['SERVER_PORT']
-        return result
+
 
 
 class WSGIHandler(base.BaseHandler):
@@ -56,6 +55,16 @@ class WSGIHandler(base.BaseHandler):
             response = http.HttpResponseBadRequest()
         else:
             response = self.get_response(request)
+
+        print('REQUEST')
+        print('is_secure:',request.is_secure())
+        print('is_ajax:',request.is_ajax())
+        print('is_xhr:',request.is_xhr())
+        print('get_host:',request.get_host())
+        print('path_info:',request.path_info)
+        print('method:',request.method)
+        print('query_string:',request.query_string)
+        print('script_name:',request.script_name)
 
         start_response(response.status, response.headers)
         return response.body
