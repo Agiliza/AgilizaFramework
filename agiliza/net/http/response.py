2e150974f46f27fab92fc6c788daf31b2b4e5a8c
Vicente Ruiz <vruiz2.0@gmail.com>
2012-02-24 10:09:42 +0100

    License notes added

--- agiliza/net/http/response.py
+++ agiliza/net/http/response.py
@@ -1,4 +1,23 @@
 """
+This file is part of Agiliza.
+
+Agiliza is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Agiliza is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Agiliza.  If not, see <http://www.gnu.org/licenses/>.
+
+
+Copyright (c) 2012 Vicente Ruiz <vruiz2.0@gmail.com>
+
+
 See http://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html#sec4.5,
     http://www.w3.org/Protocols/rfc2616/rfc2616-sec6.html#sec6.2 and
     http://www.w3.org/Protocols/rfc2616/rfc2616-sec7.html#sec7.1
