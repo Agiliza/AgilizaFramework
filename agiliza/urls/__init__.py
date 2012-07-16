9b6f8d0885c217028f02be97e6de38cce8aad636
Vicente Ruiz <vruiz2.0@gmail.com>
2012-07-16 23:33:40 +0200

    Refactoring urls

--- agiliza/urls/__init__.py
+++ agiliza/urls/__init__.py
@@ -17,3 +17,128 @@ along with Agiliza.  If not, see <http://www.gnu.org/licenses/>.
 
 Copyright (c) 2012 Alvaro Hurtado <alvarohurtado84@gmail.com>
 """
+import importlib
+
+
+def url(expression, target, layouts=None,
+        name=None, common_context_processors=[]):
+    """
+    Create a list of urls.
+    Must return one of these:
+        * [
+            (
+            'regular expression',
+            "target.function",
+            [context_processor1, context_processor2] or [],
+            'name' or None,
+            'layout_name' or None),
+
+            (),
+        ...]
+    """
+    urls=[]
+
+    if isinstance(target, str) or callable(target):
+        # if target is a String (pointing a function) or a Function
+        if layouts:
+            # if url() is called with layouts
+            for layout_name in layouts.keys():
+                urls.append(
+                    (
+                        create_url(
+                            layouts[layout_name]["url_prefix"] or "",
+                            expression
+                        ),
+                        target,
+                        common_context_processors + \
+                            layouts[layout_name]["context_processors"],
+                        name,
+                        layout_name,
+                    )
+                )
+
+        else:
+            urls.append(
+                (
+                    create_url(expression),
+                    target,
+                    common_context_processors,
+                    name,
+                    None,
+                )
+            )
+
+
+    else:
+        # if target is a include(...)
+        if layouts:
+
+            for layout_name in layouts:
+                for app_url in target:
+                    urls.append(
+                        (
+                            create_url(
+                                create_url(
+                                    layouts[layout_name]["url_prefix"] or "",
+                                    expression),
+                                app_url[0]
+                            ),
+                            app_url[1],
+                            app_url[2] + \
+                                common_context_processors + \
+                                layouts[layout_name]["context_processors"],
+                            app_url[3],
+                            layout_name,
+                        )
+                    )
+
+
+        else:
+
+            for app_url in target:
+                urls.append(
+                    (
+                        create_url(expression, app_url[0]),
+                        app_url[1],
+                        app_url[2] + common_context_processors,
+                        app_url[3],
+                        app_url[4],
+                    )
+                )
+
+    return urls
+
+
+
+def include(module_name):
+    try:
+        url_module = importlib.import_module(module_name)
+        url_patterns = getattr(url_module, 'url_patterns')
+    except ImportError:
+        pass
+    except AttributeError:
+        pass
+
+    return [ url for url in url_patterns ]
+
+
+
+def create_url(first, second=None):
+    if not first.startswith("^"):
+        first = "^" + first
+
+    if second:
+        if first.endswith("$"):
+            first = first[:-1]
+        if second.startswith("^"):
+            second = second[1:]
+        if not second.endswith("$"):
+            second = second + "$"
+    else:
+        if not first.endswith("$"):
+            first = first + "$"
+
+    if second:
+        return first + second
+    else:
+        return first
